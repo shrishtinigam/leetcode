6280714df4752e732554858b48cf461520766382
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-01 20:05:07 +0530

    Time: 13 ms (80.64%), Space: 7.8 MB (69.60%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string minWindow(string s, string t) 
+    {
+        vector<int> m(128, 0);
+        // Statistic for count of char in t
+        for (auto c : t) m[c]++;
+        // counter represents the number of chars of t to be found in s.
+        int left = 0, right = 0, counter = t.size(), minStart = 0, minLen = INT_MAX;
+        int size = s.size();
+
+        // Move right to find a valid window.
+        while (right < size) 
+        {
+            // If char in s exists in t, decrease counter
+            if (m[s[right]] > 0)
+                counter--;
+            
+            // Decrease m[s[right]]. If char does not exist in t, m[s[right]] will be negative.
+            m[s[right]]--;
+            right++;
+            
+            // When we found a valid window, move left to find smaller window.
+            while (counter == 0) 
+            {
+                if (right - left < minLen) 
+                {
+                    minStart = left;
+                    minLen = right - left;
+                }
+                m[s[left]]++;
+                // When char exists in t, increase counter.
+                if (m[s[left]] > 0)
+                    counter++;
+                
+                left++;
+            }
+        }
+        
+        if (minLen != INT_MAX)
+            return s.substr(minStart, minLen);
+        return "";
+    }
+};
