17cd82eaf52dcc6b23fb33c271a3b6f06c65a0a0
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-04 10:56:09 +0530

    Time: 183 ms (85.24%), Space: 85.1 MB (71.28%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
         vector <int> answer(temperatures.size());
         int i = temperatures.size() - 1;
         stack <int> pos;
         pos.push(i);
+        
         while(i--)
         {
             while(!pos.empty() and temperatures[pos.top()] <= temperatures[i])
