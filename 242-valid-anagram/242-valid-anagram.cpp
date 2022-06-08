aea87435d9c7787d76e9feabcc6c9d4d1bcdb7fa
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-08 16:26:47 +0530

    Time: 6 ms (88.34%), Space: 7.4 MB (46.52%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector <int> counts (26, 0);
-        vector <int> countt (26, 0);
+        // Frequency counting of characters will help to determine if two strings are anagrams.
+        // This takes O(n) time and O(1) space.
+        vector <int> counts (26, 0), countt (26, 0);
+        
         for(int i = 0; i < s.length(); i++)
             counts[int(s[i]) - 97]++;
 
