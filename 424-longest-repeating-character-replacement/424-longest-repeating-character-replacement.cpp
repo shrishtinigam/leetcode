5aff46b97158cd21ee71eee4153e561cd3b5de44
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-08 18:10:31 +0530

    Time: 18 ms (50.89%), Space: 7.1 MB (54.55%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        // res is the length of the largest window we can find. 
+        // The largest window we can find has this property: res = maxf + k
+        // maxf is the max frequency of any character we have seen so far.
+        int res = 0, maxf = 0;
+        unordered_map<int, int> count;
+        for (int i = 0; i < s.length(); ++i) {
+            maxf = max(maxf, ++count[s[i]]);
+            if (res - maxf < k)
+                res++;
+            else
+                count[s[i - res]]--; // decreasing the count of the characters that are now outside the window
+        }
+        return res;
+    }
+};
