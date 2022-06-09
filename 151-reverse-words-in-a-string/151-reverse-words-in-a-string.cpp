4d10be0c7f2cee2758a16a0611731826ba673a00
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-09 17:53:44 +0530

    Time: 13 ms (31.64%), Space: 8.1 MB (36.87%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    string reverseWords(string &s) 
+    string reverseWords(string s) 
     {
-        reverse(s.begin(), s.end());
-        int storeIndex = 0;
-        for (int i = 0; i < s.size(); i++) 
+        vector <string> arr;
+        int i = 0, j = 0;
+        string temp = "";
+        while(i < s.length())
         {
-            if (s[i] != ' ') 
+        // Check for space and push in the array whenever space occured
+           if(s[i] != ' ')
+               temp += s[i];
+            else if(temp != "")
             {
-                if (storeIndex != 0) s[storeIndex++] = ' ';
-                int j = i;
-                
-                while (j < s.size() && s[j] != ' ') 
-                    s[storeIndex++] = s[j++];
-                
-                reverse(s.begin() + storeIndex - (j - i), s.begin() + storeIndex);
-                i = j;
+               arr.push_back(temp);
+               temp = "";
             }
+            i++;
         }
-        s.erase(s.begin() + storeIndex, s.end());
-        return s;
+        // as we check till the s.length() so we miss the last word.
+        // So push it outside the loop with the check that it's not a space
+        
+        if(temp != "")
+            arr.push_back(temp);
+        
+        // Reverse the arr as we need to return the reversed string
+        reverse(arr.begin(), arr.end());
+        
+        string ans = "";
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(i!=arr.size()-1)
+            {
+                ans += arr[i];
+                ans += " ";
+            }
+            else
+                ans += arr[i];
+        }
+        return ans;
     }
 };
