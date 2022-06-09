1b56f1293eede39496062ef514c7bb2dd1c36448
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-09 23:21:04 +0530

    Time: 3 ms (99.75%), Space: 7 MB (97.74%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
-    bool ispallin(string ans){
-        int i=0;
-        int j=ans.size()-1;
-        while(i<j){
-            if(ans[i]!=ans[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
     string longestPalindrome(string s) {
-     if (s.empty()) return "";
-    if (s.size() == 1) return s;
-    int min_start = 0, max_len = 1;
-    for (int i = 0; i < s.size();) {
-      if (s.size() - i <= max_len / 2) break;
-      int j = i, k = i;
-      while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
-      i = k+1;
-      while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
-      int new_len = k - j + 1;
-      if (new_len > max_len) { min_start = j; max_len = new_len; }
-    }
-    return s.substr(min_start, max_len);  
+        
+        // edge cases
+        if(s.length() == 0 || s.length() == 1) return s;
+        
+        // to hold max len and its starting index
+        int maxLenBeginIndex = 0;
+        int maxLen = 1;
+        
+        int mid = 0;
+        while(mid < s.length()){
+            
+            // calculating middle window
+            
+            int midBegin = mid;
+            int midEnd = mid;
+            
+            // handling even length palindromes; the middlemost chars will trivially match in even length case
+            // so expand the middle window as long as possible
+            while( midEnd + 1 < s.length() && s[midEnd] == s[midEnd + 1]){ midEnd++ ; }
+            
+            // for next iteration
+            mid = midEnd + 1;
+            
+            // starting comparison in left and right windows
+            
+            // in case of odd len palindrome; both start from mid;
+            // in case of even; midBegin and midEnd handles it all
+            int leftWindow = midBegin;
+            int rightWindow = midEnd;
+            
+            // expand the windows left and right simultaneously
+            while(leftWindow - 1 >= 0 && rightWindow + 1 < s.length() && s[leftWindow - 1] == s[rightWindow + 1]){
+                    leftWindow--;
+                    rightWindow++;
+            }
+            
+            // update maxLen is currLen > maxLen
+            int currLen = rightWindow - leftWindow + 1;
+            if( currLen  > maxLen ){
+                maxLenBeginIndex = leftWindow;
+                maxLen = currLen;
+            }
+        }
+        
+        return s.substr(maxLenBeginIndex, maxLen);
     }
         
 };
