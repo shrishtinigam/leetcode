62a2de3bb2dd50ab18feecacc1f0bb7726f86aec
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-11 18:52:29 +0530

    Time: 28 ms (20.02%), Space: 8 MB (37.74%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) 
+    {
+        unordered_map <char, int> roman_numeral;
+        roman_numeral['I'] = 1;
+        roman_numeral['V'] = 5;
+        roman_numeral['X'] = 10;
+        roman_numeral['L'] = 50;
+        roman_numeral['C'] = 100;
+        roman_numeral['D'] = 500;
+        roman_numeral['M'] = 1000;
+        
+        int sum = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(i < s.size() - 1 and roman_numeral[s[i]] < roman_numeral[s[i + 1]])
+            {
+                sum -= roman_numeral[s[i]];
+                continue;
+            }
+            sum += roman_numeral[s[i]];
+        }
+        return sum;
+    }
+};
