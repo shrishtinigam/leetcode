8740795da1fac532c7a2c1f50e2f82ce29c6f31e
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-12 11:14:44 +0530

    Time: 12 ms (88.23%), Space: 8.8 MB (39.59%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> u_map;
+    bool canConstruct(string ransomNote, string magazine) 
+    { 
+        // 97 = int('a')
+        int freq[26] = {0};
+        for(int i = 0; i < magazine.size(); i++)
+            freq[int(magazine[i]) - 97]++;
+        
+        for(int i = 0; i < ransomNote.size(); i++)
+            freq[int(ransomNote[i]) - 97]--;
+        
+        for(int i = 0; i < 26; i++)
+            if(freq[i] < 0)
+                return false;
+        return true;
+    }
+};
+
+/*
+unordered_map<char, int> u_map;
 
         for(int i = 0; i < magazine.size(); i++)
             u_map[magazine[i]]++;
@@ -11,5 +28,4 @@ public:
             if(i->second < 0)
                 return false;
         return true;
-    }
-};
+*/
