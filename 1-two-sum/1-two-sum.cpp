b686a9144a8619c9a8eb4c854b2e9d7e061a4347
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-06-12 11:44:21 +0530

    Time: 11 ms (88.96%), Space: 10.8 MB (52.95%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -9,29 +9,30 @@ public:
                     return i;
             return -1;
         }
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> x = nums;        
-        sort(x.begin(), x.end());
-        int a = 0, b = x.size() - 1;
-        
-        while(a < b)
+        vector<int> twoSum(vector<int>& nums, int target) 
         {
-            if(x[a] + x[b] == target)
-                break;
-            if(x[a] + x[b] > target)
-                b--;
+            vector<int> x = nums;        
+            sort(x.begin(), x.end());
+            int a = 0, b = x.size() - 1;
+
+            while(a < b)
+            {
+                if(x[a] + x[b] == target)
+                    break;
+                if(x[a] + x[b] > target)
+                    b--;
+                else
+                    a++;
+            }
+            a = x[a];
+            b = x[b];
+            int a_i = linearsearch(nums, 0, a);
+            int b_i = 0;
+            if(a == b)
+                b_i = linearsearch(nums, a_i + 1, b);
             else
-                a++;
+                b_i = linearsearch(nums, 0, b);
+            vector <int> ans = {a_i, b_i};
+            return ans;
         }
-        a = x[a];
-        b = x[b];
-        int a_i = linearsearch(nums, 0, a);
-        int b_i = 0;
-        if(a == b)
-            b_i = linearsearch(nums, a_i + 1, b);
-        else
-            b_i = linearsearch(nums, 0, b);
-        vector <int> ans = {a_i, b_i};
-        return ans;
-    }
 };
