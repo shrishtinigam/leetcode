8b61887de1fc037323a51e88a5eb5cf87ae834b6
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-07-06 20:23:11 +0530

    Time: 21 ms (53.38%), Space: 17.8 MB (39.89%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -1,3 +1,6 @@
+// 1 While stack is not empty and the filled with positive numbers, remove all the positive numbers from the top that are less than absolute value of the current number.
+// 2 While stack is not empty and stack top is equal to absolute value of current number. 
+// 3 If stack is empty or stack top is negative (in which case all the stack elements are negative), push the negative number to current stack.
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& ast) {
@@ -6,13 +9,13 @@ public:
         for(int i = 0; i < n; i++) {
             if(ast[i] > 0 || s.empty())
                 s.push(ast[i]);
-            else{
-                while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i]))
+            else{ 
+                while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i])) // 1
                     s.pop();
-                if(!s.empty() and s.top() == abs(ast[i])) 
+                if(!s.empty() and s.top() == abs(ast[i])) // 2
                     s.pop();
                 else 
-                    if(s.empty() || s.top() < 0) 
+                    if(s.empty() || s.top() < 0) // 3
                         s.push(ast[i]);
             }
         }
