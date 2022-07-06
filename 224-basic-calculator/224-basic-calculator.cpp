e0964b448450ef79a78f3527d7a25d4d28c1fa47
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-07-06 18:37:14 +0530

    Time: 16 ms (45.50%), Space: 8.1 MB (72.89%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    int calculate(string s) {
+        int sum = 0;
+        int sign = 1;
+        int j = 0;
+        stack<int> st;
+        
+        while(j < s.length())
+        { 
+            if(s[j] == '+')
+                sign = 1;
+            else if(s[j] == '-')
+                sign = -1;
+            else if(isdigit(s[j]))
+            {
+                int num = s[j] - '0';
+                while(j + 1 < s.length() && isdigit(s[j + 1]))
+                {
+                    num = num * 10 + (s[j + 1] - '0');
+                    j++;
+                }
+                sum += num * sign;
+            }
+            else if(s[j] == '(')
+            {
+                st.push(sum);
+                st.push(sign);
+                sum = 0;
+                sign = 1;
+            }
+            else if(s[j]==')')
+            {
+                int xsign = st.top();
+                st.pop();
+                int xsum = st.top();
+                st.pop();
+                sum = sum * xsign + xsum;
+            }
+            j++;
+        }
+        return sum;
+    }
+};
+
