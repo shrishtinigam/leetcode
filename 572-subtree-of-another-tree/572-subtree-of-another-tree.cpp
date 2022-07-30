622b92696728048da5c7288de54896d93ddbf684
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-07-30 07:42:04 +0530

    Time: 47 ms (32.57%), Space: 28.9 MB (57.23%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -15,19 +15,24 @@ public:
     
     bool isSameTree(TreeNode* p, TreeNode* q) 
     {
-        if((p != NULL and q == NULL) or (p == NULL and q != NULL))
-            return false;
-        if(p == NULL and q == NULL)
+        if(!p and !q)
             return true;
+
+        if(!p or !q)
+            return false;
+        
         if(p->val != q->val)
             return false;
+           
         if(isSameTree(p->right, q->right) and isSameTree(p->left, q->left))
             return true;
         return false;
     }
     
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if((root != NULL and subRoot == NULL) or (root == NULL and subRoot != NULL))
+        //if(!root and !subRoot)
+            //return true;
+        if(!root or !subRoot)
             return false;
 
         if(root->val == subRoot->val and isSameTree(root, subRoot))
