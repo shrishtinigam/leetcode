938c2c97425b19bcf91244078443707ef48c8c6d
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-08-03 07:05:54 +0530

    Time: 10 ms (89.99%), Space: 8.1 MB (17.02%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -6,6 +6,10 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) 
@@ -23,3 +27,25 @@ public:
         return false;
     }
 };
+
+/*
+bool hasCycle(ListNode *head) {
+	
+        if(head == NULL)
+            return false;
+        
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow)
+                return true;
+        }
+        
+        return false;
+    }
+*/
