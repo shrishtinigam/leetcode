da7ee2c12c55895cc91cc59f212e25038659b688
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-08-06 08:03:20 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (93.30%) - LeetHub

---
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode * leftprev = dummy;
+        ListNode * cur = head;
+        for(int i = 0; i < left-1; i++){
+            leftprev = cur;
+            cur = cur->next;
+        }
+        
+        ListNode * prev = NULL;
+        for(int i = 0; i < (right - left + 1); i++){
+            ListNode * temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        
+        leftprev->next->next = cur;
+        leftprev->next = prev;
+        
+        return dummy->next;
+    }
+};
+
+/*
+        if(head->next == NULL)
+            return head;
+        ListNode * cur = head;
+        left--;        
+        left--;
+        for(int i = 0; i < left; i++){
+            cur = cur->next;
+        }
+        cout << cur->val<<"\n";
+        
+        int count = left;
+        if(left+2 == 1)
+            count = 0;
+        ListNode * ptr = cur;
+        ListNode * ptr2 = cur->next;
+        ListNode* prev = NULL; 
+        ListNode * temp;
+        while(count < right){
+            temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
+            count++;
+        }
+        cout<<prev->val<<"\n";        
+        cout<<ptr->val<<"\n";
+        cout<<ptr2->val<<"\n";
+
+        /*
+        //ListNode * x = ptr->next;
+        ptr->next = prev;
+        ptr2->next = cur;
+        return head;
+*/
