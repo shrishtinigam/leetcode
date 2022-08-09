1d786998f8e99575f2e13215d622d13e0c882213
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-08-09 18:12:21 +0530

    Time: 3 ms (60.71%), Space: 7 MB (92.54%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -1,7 +1,7 @@
 class Twitter {
 private:    
-   unordered_map<int, set<int>> fo;
-   unordered_map<int, vector<pair<int, int>>> t;
+   unordered_map<int, set<int>> followMap;
+   unordered_map<int, vector<pair<int, int>>> tweetMap;
    long long time; 
 
 public:
@@ -12,37 +12,37 @@ Twitter() {
 
 /** Compose a new tweet. */
 void postTweet(int userId, int tweetId) {
-    t[userId].push_back({time++, tweetId});
+    tweetMap[userId].push_back({time++, tweetId});
 }
 
 /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
 vector<int> getNewsFeed(int userId) {
     priority_queue<pair<int, int>> maxHeap; 
-    for (auto it=t[userId].begin();it!=t[userId].end();++it)
+    for (auto it = tweetMap[userId].begin(); it != tweetMap[userId].end(); ++it)
         maxHeap.push(*it);
-    for (auto it1=fo[userId].begin();it1!=fo[userId].end();++it1){
-        int usr = *it1; // get target user
-        for (auto it2=t[usr].begin();it2!=t[usr].end();++it2)
+    for (auto it1 = followMap[userId].begin(); it1 != followMap[userId].end(); ++it1){
+        int usr = *it1;
+        for (auto it2 = tweetMap[usr].begin(); it2 != tweetMap[usr].end(); ++it2)
             maxHeap.push(*it2);
     }   
-    vector<int> res;
+    vector<int> recentTweets;
     while(maxHeap.size()>0) {
-        res.push_back(maxHeap.top().second);
-        if (res.size()==10) break;
+        recentTweets.push_back(maxHeap.top().second);
+        if (recentTweets.size()==10) break;
         maxHeap.pop();
     }
-    return res;
+    return recentTweets;
 }
 
 /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
 void follow(int followerId, int followeeId) {
     if (followerId != followeeId)
-        fo[followerId].insert(followeeId);
+        followMap[followerId].insert(followeeId);
 }
 
 /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
 void unfollow(int followerId, int followeeId) {
-    fo[followerId].erase(followeeId);
+    followMap[followerId].erase(followeeId);
 }
     
 
