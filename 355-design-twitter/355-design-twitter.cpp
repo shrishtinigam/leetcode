c9b8a0ea724adbd7eda0aea993479960941f7a0b
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-08-09 18:13:50 +0530

    Time: 6 ms (23.47%), Space: 7.3 MB (48.42%) - LeetHub

--- 355-design-twitter/355-design-twitter.cpp
+++ 355-design-twitter/355-design-twitter.cpp
@@ -16,7 +16,9 @@ void postTweet(int userId, int tweetId) {
     tweetMap[userId].push_back({time++, tweetId});
 }
 
-/** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+/** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+Can be made better if we use the "k sorted lists optimum pointer based addition to heap" method.
+*/
 vector<int> getNewsFeed(int userId) {
     priority_queue<pair<int, int>> maxHeap; 
     for (auto it = tweetMap[userId].begin(); it != tweetMap[userId].end(); ++it)
