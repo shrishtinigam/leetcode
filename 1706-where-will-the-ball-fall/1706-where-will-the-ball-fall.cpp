f5b89a86153155e35e453d6bf8f9328fa3a426ad
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-11-01 22:31:45 +0530

    Time: 28 ms (89.01%), Space: 13.2 MB (80.92%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -2,43 +2,38 @@ class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
         int balls = grid[0].size();
-        int flag = 0;
         vector <int> answers;
-        //cout << grid.size();
-        //cout << "\n";
         for(int j1 = 0; j1 < balls; j1++){
             int i = 0;
             int j = j1;
-            int dirn = 0;
             while(true){
-                //cout << i;
                 if(i >= grid.size()){
                     answers.push_back(j);
                     break;
                 } 
-                if(j == 0 and grid[i][j] == -1){
+                if(j == 0 and grid[i][j] == - 1){ // STUCK ON LEFT COLUMN
                     answers.push_back(-1);
                     break;
                 }
-                if(j == balls-1 and grid[i][j] == 1){
+                if(j == balls-1 and grid[i][j] == 1){ // STUCK ON RIGHT COLUMN
                     answers.push_back(-1);
                     break;
                 }
-                if(j < balls and grid[i][j] == 1 and grid[i][j] != grid[i][j+1]){
+                if(j < balls and grid[i][j] == 1 and grid[i][j] != grid[i][j+1]){ // STUCK IN THE MIDDLE
                     answers.push_back(-1);
                     break;
                 }
-                if(j > 0 and grid[i][j] == -1 and grid[i][j] != grid[i][j-1]){
+                if(j > 0 and grid[i][j] == -1 and grid[i][j] != grid[i][j-1]){ // STUCK IN THE MIDDLE
                     answers.push_back(-1);
                     break;
                 }
                 
-                if(grid[i][j] == 1){
+                if(grid[i][j] == 1)
                     j++;
-                }
-                if(grid[i][j] == -1){
+                
+                if(grid[i][j] == -1)
                     j--;
-                }
+                
                 i++;
             }
         }
