bcc73611f6e2419a2137aaaf3b51561db851778c
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2022-11-02 22:16:38 +0530

    Time: 3 ms (45.67%), Space: 6.6 MB (42.79%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,35 +1,46 @@
+// gODDAMNIT It was BFS all along, not my code 
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-	//st holds all valid mutations
-	unordered_set<string> st{bank.begin(),bank.end()};
+        
+	// st holds all valid mutations
+	unordered_set<string> st {bank.begin(),bank.end()};
+        
 	//if end mutaion is not in list return -1;
 	if(!st.count(end)) return -1;
+        
 	//start BFS by pushing the starting mutation
-	queue<string> Q;
+	queue <string> Q;
 	Q.push(start);
-	int steps=0,s;
-	string cur,t;
+        
+	int steps = 0, s;
+	string cur, t;
+        
 	while(!Q.empty()){
-		s=Q.size();
+        
+		s = Q.size();
 		while(s--){
-			cur=Q.front();
+            
+			cur = Q.front();
 			Q.pop();
+            
 			//If we reach end mutation
-			if(cur==end) return steps;
+			if(cur == end) return steps;
+            
 			//We erase the cur mutation in order to avoid redundant checking
 			st.erase(cur);
+            
 			//as the length of mutation is 8 and it can take A,C,G,T
 			//at each index we check the possibility of mutation by replcaing it with A,C,G,T
-			for(int i=0;i<8;i++){
-				t=cur;
-				t[i]='A';
+			for(int i = 0; i < 8; i++){
+				t = cur;
+				t[i] = 'A';
+				if(st.count(t)) Q.push(t); // if it is in the bank, add it to the queue
+				t[i] = 'C';
 				if(st.count(t)) Q.push(t);
-				t[i]='C';
+				t[i] = 'G';
 				if(st.count(t)) Q.push(t);
-				t[i]='G';
-				if(st.count(t)) Q.push(t);
-				t[i]='T';
+				t[i] = 'T';
 				if(st.count(t)) Q.push(t);
 			}
 		}
