bcbec013740bd6dfcf467769aa0ec23d75a02ff5
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2023-07-20 21:52:12 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -25,10 +25,34 @@ public:
             return dp[n][sum] = subsetSum(arr, sum, n-1, dp);
         return dp[n][sum] = subsetSum(arr, sum, n-1, dp) or subsetSum(arr, sum-arr[n-1],n-1,dp);
     }
-    bool isSubsetSum(vector<int>arr, int sum){
+    /*bool isSubsetSum(vector<int>arr, int sum){
         vector<vector<int>>dp(arr.size()+1,vector<int>(sum+1,-1));
         return subsetSum(arr,sum,arr.size(),dp);
+    }*/
+    
+    bool isSubsetSum(vector<int>arr, int sum){
+        int n = arr.size();
+        int dp[n+1][sum+1];
+        
+        for(int i = 1; i < sum+1; i++)
+            dp[0][i] = 0;
+        for(int i = 0; i < n+1; i++)
+            dp[i][0] = 1;
+        
+        
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < sum+1; j++){
+                if(arr[i-1] > j)
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i-1]];
+            }
+        }
+        
+        return dp[n][sum];
+        
     }
+    
 };
 
 
@@ -49,7 +73,7 @@ public:
         if(dp[n][sum]!=-1){
             return dp[n][sum];
         }
-        if(arr[n-1]>sum){
+        if(arr[n-1]>sum){ 
             return dp[n][sum]=checkSubsetSum(arr,sum,n-1,dp);
         }
         else{
