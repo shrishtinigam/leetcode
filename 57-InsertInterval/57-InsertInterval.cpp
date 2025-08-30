0d286c3fb2ecab86b98707688f8a40313bbafa3c
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:20:46 +0530

    Create 57-InsertInterval

---
+++ 57-InsertInterval/57-InsertInterval.cpp
@@ -0,0 +1,51 @@
+// Last updated: 8/30/2025, 8:20:45 PM
+class Solution {
+public:
+    
+    vector<vector<int>> insert(vector<vector<int>>& I, vector<int>& newInterval) 
+    {
+        auto it = lower_bound(I.begin(), I.end(), newInterval);
+        int pos = it - I.begin();
+        
+        I.insert(it, newInterval);
+        
+        if(I.size() == 0)
+            return I;
+        
+        int start = (pos <= 0) ? 0 : pos - 1;
+        int R = start;
+        
+        for(int j = start; j < I.size(); j++)
+        {
+            vector <int> i = I[j];
+            if(i[0] <= I[R][1])                  
+                I[R][1] = max(I[R][1], i[1]);
+            else
+                I[++R] = i; 
+        }
+        
+        I.resize(R+1); 
+        return I;
+    }
+};
+
+
+/* OLD SOLUTION
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& I, vector<int>& newInterval) {
+        auto it = lower_bound(I.begin(), I.end(), newInterval);
+        I.insert(it, newInterval);
+        int R = 0; 
+        for(auto i : I)
+            if(i[0] <= I[R][1])                  
+                I[R][1] = max(I[R][1], i[1]);
+            else
+                I[++R] = i; 
+        I.resize(R+1); 
+        return I;
+        
+        for
+    }
+};
+*/
