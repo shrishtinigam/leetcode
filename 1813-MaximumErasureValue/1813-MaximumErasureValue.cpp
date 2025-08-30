30b0aa9d7b10a386e633f7299768208d7aacc4ad
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:18:09 +0530

    Create 1813-MaximumErasureValue

---
+++ 1813-MaximumErasureValue/1813-MaximumErasureValue.cpp
@@ -0,0 +1,27 @@
+// Last updated: 8/30/2025, 8:18:09 PM
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) 
+        
+    {
+        unordered_map <int, bool> umap;
+        int max_res = 0;
+        int curr = 0;
+        int i = 0;
+        int j = 0;
+        while(j < nums.size())
+        {
+            while(umap.find(nums[j]) != umap.end())
+            {
+                curr -= nums[i];
+                umap.erase(nums[i]);
+                i++;
+            }
+            umap[nums[j]] = true;
+            curr += nums[j];
+            max_res = max (max_res, curr);
+            j++;
+        }
+        return max_res;
+    }
+};
