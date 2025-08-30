1a4ac6a372a7845f0ab8b4bd6930ac858eed3401
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:19:17 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/30/2025, 8:19:17 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // Frequency counting of characters will help to determine if two strings are anagrams.
+        // This takes O(n) time and O(1) space.
+        vector <int> counts (26, 0), countt (26, 0);
+        
+        for(int i = 0; i < s.length(); i++)
+            counts[int(s[i]) - 97]++;
+
+        for(int i = 0; i < t.length(); i++)
+            countt[int(t[i]) - 97]++;
+        
+        if(counts == countt)
+            return true;
+        return false;
+    }
+};
