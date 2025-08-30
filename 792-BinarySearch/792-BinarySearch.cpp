74a56e738576d06bf82a7c5fb78831c5c1211dd7
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:18:31 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/30/2025, 8:18:31 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo <= hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (nums[mi] == target) return mi;
+            else if (nums[mi] < target) lo = mi + 1;
+            else hi = mi - 1;
+        }
+        return -1;
+    }
+};
