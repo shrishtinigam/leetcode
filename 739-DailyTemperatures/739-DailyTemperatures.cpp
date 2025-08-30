ee8759be1c1954f713c8054431510f362441f4a0
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:18:35 +0530

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/30/2025, 8:18:34 PM
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        vector <int> answer(temperatures.size());
+        int i = temperatures.size() - 1;
+        stack <int> pos;
+        pos.push(i);
+        
+        while(i--)
+        {
+            while(!pos.empty() and temperatures[pos.top()] <= temperatures[i])
+                pos.pop();
+            answer[i] = pos.empty() ? 0 : pos.top() - i;
+            pos.push(i);
+        }
+        return answer;
+    }
+};
