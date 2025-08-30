f6164b12ac0b851124264399283c92d16b0a4600
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:18:56 +0530

    Create 438-FindAllAnagramsInAString

---
+++ 438-FindAllAnagramsInAString/438-FindAllAnagramsInAString.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/30/2025, 8:18:55 PM
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) 
+    {
+        if(s.size() < p.size()) return {};
+        vector <int> count_s (26, 0), count_p(26, 0);
+        for(int i = 0; i < p.size(); i++)
+        {
+            count_p[int(p[i])-97]++;            
+            count_s[int(s[i])-97]++;
+        }
+        
+        vector <int> res;
+        
+        if(count_p == count_s)
+                res.push_back(0);
+        for(int i = p.size(); i < s.size(); i++)
+        {
+            count_s[int(s[i-p.size()])-97]--;
+            count_s[int(s[i])-97]++;
+     
+            if(count_p == count_s)
+                res.push_back(i-p.size()+1);
+        }
+        return res;
+    }
+};
