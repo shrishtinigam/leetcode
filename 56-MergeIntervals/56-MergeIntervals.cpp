99e229b75b73cf2c8dbb24ff9676171bc877fe3f
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:20:47 +0530

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.cpp
@@ -0,0 +1,39 @@
+// Last updated: 8/30/2025, 8:20:46 PM
+class Solution {
+public:
+     vector<vector<int>> merge(vector<vector<int>>& I) 
+     {
+        //sort(begin(I), end(I));
+        sort(I.begin(), I.end());
+        int R = 0; 
+        for(auto i : I)
+            if(i[0] <= I[R][1])                  
+                I[R][1] = max(I[R][1], i[1]);
+            else
+                I[++R] = i; 
+        I.resize(R+1); 
+        return I;
+    }
+};
+/*
+With Comments
+
+class Solution {
+public:
+     vector<vector<int>> merge(vector<vector<int>>& I) 
+     {
+        sort(begin(I), end(I));
+        int R = 0; // Rth index, Index of the interval we will compare all new intervals with. 
+         
+         // If Rth and ith interval are "mergeable", we merge them into the Rth interval, other wise we will increase R and make the newest "unmergable" interval the Rth interval.
+         
+        for(auto i : I)
+            if(i[0] <= I[R][1]) // If the end time of Rth interval is   greater than or equal to start time of the i th interval                   
+                I[R][1] = max(I[R][1], i[1]); // Merge Intervals
+            else
+                I[++R] = i; // Increase R and replace the next interval with the i th interval. In the next iterations we will compare the new intervals to this interval
+        I.resize(R+1); // Delete all intervals after the Rth interval
+        return I;
+    }
+};
+*/
