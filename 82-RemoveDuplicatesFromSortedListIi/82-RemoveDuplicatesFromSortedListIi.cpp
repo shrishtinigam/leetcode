b8b9e6151bcbe329c9b359853520c0e31dae5c92
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:20:25 +0530

    Create 82-RemoveDuplicatesFromSortedListIi

---
+++ 82-RemoveDuplicatesFromSortedListIi/82-RemoveDuplicatesFromSortedListIi.cpp
@@ -0,0 +1,66 @@
+// Last updated: 8/30/2025, 8:20:25 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head)
+            return head;
+        ListNode * cur = head;
+        ListNode * dummy = new ListNode(0, head);
+        ListNode * last = dummy;
+        ListNode * next;
+        while(cur->next)
+        {
+            next = cur->next;
+            
+            if(next and cur->val != next->val){
+                last = cur;
+                cur->next = next;
+                cur = cur->next;
+                continue;
+            }
+                
+            
+            while(next and cur->val == next->val){
+                next = next->next;
+            }
+            
+            if(!next){
+                last->next = next;
+                return dummy->next;
+            }
+            
+            last->next = next;
+            cur = next;
+        }
+        
+        last = cur;
+        return dummy->next;
+    }
+};
+
+/*
+ListNode * cur = head;
+        
+        while(cur)
+        {
+            ListNode * next = cur->next;
+            
+            while(next and cur->val == next->val)
+                next = next->next;
+            
+            cur->next = next;
+            cur = cur->next;
+        }
+        
+        return head;
+*/
