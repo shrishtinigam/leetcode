f9e194fe9b7e3ff2513729421e77db60f6090779
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:19:46 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.cpp
@@ -0,0 +1,44 @@
+// Last updated: 8/30/2025, 8:19:45 PM
+class Solution {
+public:
+    string reverseWords(string s) 
+    {
+        
+        vector <string> arr;
+        int i = 0, j = 0;
+        string temp = "";
+        while(i < s.length())
+        {
+        // Check for space and push in the array whenever space occured
+           if(s[i] != ' ')
+               temp += s[i];
+            else if(temp != "")
+            {
+               arr.push_back(temp);
+               temp = "";
+            }
+            i++;
+        }
+        // as we check till the s.length() so we miss the last word.
+        // So push it outside the loop with the check that it's not a space
+        
+        if(temp != "")
+            arr.push_back(temp);
+        
+        // Reverse the arr as we need to return the reversed string
+        reverse(arr.begin(), arr.end());
+        
+        string ans = "";
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(i!=arr.size()-1)
+            {
+                ans += arr[i];
+                ans += " ";
+            }
+            else
+                ans += arr[i];
+        }
+        return ans;
+    }
+};
