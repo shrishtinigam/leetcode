3cdcf093134a825f9106869824c96c32fbc8df15
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-30 20:19:54 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.cpp
@@ -0,0 +1,66 @@
+// Last updated: 8/30/2025, 8:19:53 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) 
+    {
+        ListNode *fast = head, *slow = head;
+        while(fast and fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+
+    /*
+    bool hasCycle(ListNode *head) 
+    {
+        ListNode* tortoise = head, *hare = head;
+        while(tortoise != NULL and hare != NULL){
+            tortoise = tortoise->next;
+            hare = hare->next;
+            if(hare == NULL)
+                return false;
+            hare = hare->next;
+            if(tortoise == hare)
+                return true;
+        }
+        return false;
+    }
+    */
+};
+
+/*
+bool hasCycle(ListNode *head) {
+	
+        if(head == NULL)
+            return false;
+        
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow)
+                return true;
+        }
+        
+        return false;
+    }
+*/
