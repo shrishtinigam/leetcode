a230f069aea94ae3a7e154c2fd9dcb9925bfb581
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-08-31 19:33:37 +0530

    File updated at 8/31/2025, 7:33:36 PM

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,15 +1,9 @@
-# Last updated: 8/31/2025, 7:30:12 PM
+# Last updated: 8/31/2025, 7:33:36 PM
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        #if digits[-1] < 9:
-        #    digits[-1]+=1
-        #    return digits
-        for i in range(len(digits)-1, -1, -1):
-            if digits[i] == 9:
-                digits[i] = 0
-            else:
-                digits[i]+=1
-                break
-        if digits[0] == 0:
-            digits.insert(0, 1)
-        return digits
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
