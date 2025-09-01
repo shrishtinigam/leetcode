9c6ae0e90bb312db2aa8708ebf046be67c4cce72
Meher Shrishti Nigam <mehershrishtinigam@gmail.com>
2025-09-01 20:19:12 +0530

    File updated at 9/1/2025, 8:19:11 PM

--- 228-SummaryRanges/228-SummaryRanges.py
+++ 228-SummaryRanges/228-SummaryRanges.py
@@ -1,20 +1,15 @@
-# Last updated: 9/1/2025, 8:13:18 PM
+# Last updated: 9/1/2025, 8:19:11 PM
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        summary_ranges = []
-        if len(nums) == 1:
-            return [f"{nums[0]}"]
-        if len(nums) == 0:
+        if not nums:
             return []
-        cur_a, cur_b = nums[0], nums[0]
-        for i in range(len(nums)):
-            if i != len(nums) - 1 and nums[i] + 1 == nums[i+1]:
-                cur_b = nums[i+1]
+        ranges = []
+        start = prev = nums[0]
+        for num in nums[1:]:
+            if num == prev + 1:  # continue the range
+                prev = num
             else:
-                if cur_a == cur_b:
-                    summary_ranges += [f"{cur_a}"]
-                else:
-                    summary_ranges += [f"{cur_a}->{cur_b}"]
-                if i != len(nums) - 1:
-                    cur_a, cur_b = nums[i+1], nums[i+1]
-        return summary_ranges
+                ranges.append(str(start) if start == prev else f"{start}->{prev}")
+                start = prev = num
+        ranges.append(str(start) if start == prev else f"{start}->{prev}")
+        return ranges
